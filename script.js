d58f195742dbcbc1012a88f6f9f4e522ec1ebb9a
Adam123569 <codebraver1@codebrave.org>
2024-12-26 18:17:35 +0200

    First time using git

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const projects = document.querySelectorAll('.project');
+    projects.forEach(project => {
+        project.addEventListener('mouseover', () => {
+            project.style.transform = 'scale(1.05)';
+        });
+        project.addEventListener('mouseout', () => {
+            project.style.transform = 'scale(1)';
+        });
+    });
+});
